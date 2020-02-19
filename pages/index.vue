0d4f566d1a8c31d98f518a53884d9b94eefdf9bd
面川泰明 <omokawa@senk-inc.co.jp>
2020-02-19 10:45:43 +0900

    Change for qiita api

--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,10 @@
         <li v-for="item in items" :key="item.id">
           <h4>
             <span>{{item.title}}</span>
-            <small> by </small>
+            <small> by {{item.user.id}}</small>
           </h4>
+          <div>{{item.body.slice(0,130)}}......</div>
+          <p><a :href="item.url">{{item.url}}</a></p>
         </li>
       </ul>
     </div>
@@ -15,23 +17,12 @@
 </template>
 
 <script>
-import AppLogo from "~/components/AppLogo.vue"
-
 export default {
-  components: {
-    AppLogo
-  },
-
-  async mounted() {
-    console.log(
-      JSON.stringify(
-        await this.$axios.$get(
-          "https://qiita.com/api/v2/items?query=tag:nuxt.js"
-        ),
-        true,
-        "  "
-      )
-    )
+  async asyncData({app}) {
+    const items = await app.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js')
+    return {
+      items
+    }
   }
 }
 </script>
@@ -39,31 +30,21 @@ export default {
 <style>
 .container {
   min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  padding: 16px;
 }
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+h3 {
+  margin: 16px 0;
+  padding: 8px 0;
+  border-bottom: solid 1px #e5e5e5;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+li + li {
+  margin: 16px 0;
 }
 
-.links {
-  padding-top: 15px;
+p {
+  margin: 8px 0;
 }
+
 </style>
--- plugins/axios.js
+++ plugins/axios.js
@@ -1,5 +1,6 @@
 export default function({ $axios }) {
   $axios.onRequest(config => {
+    config.headers.common["Authorization"] = "hoge";
     if (process.env.QIITA_TOKEN) {
       config.headers.common["Authorization"] = process.env.QIITA_TOKEN;
     }
