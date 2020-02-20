4fbc792df4df670448e8ad46a6c8b0520d7e9ea6
面川泰明 <omokawa@senk-inc.co.jp>
2020-02-20 10:46:56 +0900

    Format axios code

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,9 @@
 export default function({ $axios }) {
   $axios.onRequest(config => {
     if (process.env.QIITA_TOKEN) {
-      config.headers.common["Authorization"] = `Bearer ${process.env.QIITA_TOKEN}`;
+      config.headers.common[
+        "Authorization"
+      ] = `Bearer ${process.env.QIITA_TOKEN}`;
     }
     // console.log(config.headers);
     return config;
