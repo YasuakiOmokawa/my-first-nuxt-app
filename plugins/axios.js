3bcb85de80e62fbb4aeccdb2395f843d237a4238
面川泰明 <omokawa@senk-inc.co.jp>
2020-02-20 10:55:17 +0900

    New change Qiita API

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,8 +1,11 @@
 export default function({ $axios }) {
   $axios.onRequest(config => {
     if (process.env.QIITA_TOKEN) {
-      config.headers.common["Authorization"] = process.env.QIITA_TOKEN;
+      config.headers.common[
+        "Authorization"
+      ] = `Bearer ${process.env.QIITA_TOKEN}`;
     }
+    // console.log(config.headers);
     return config;
   });
 }
