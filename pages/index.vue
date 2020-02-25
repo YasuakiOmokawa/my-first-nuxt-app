f1c50bfe909ea4e260f1834a96d95a7cd6fa5bbc
面川泰明 <omokawa@senk-inc.co.jp>
2020-02-25 19:15:52 +0900

    Add link to user detail page

--- pages/index.vue
+++ pages/index.vue
@@ -6,10 +6,15 @@
         <li v-for="item in items" :key="item.id">
           <h4>
             <span>{{item.title}}</span>
-            <small> by {{item.user.id}}</small>
+            <small>
+              <span>by</span>
+              <nuxt-link :to="`/users/${item.user.id}`">{{item.user.id}}</nuxt-link>
+            </small>
           </h4>
-          <div>{{item.body.slice(0,130)}}......</div>
-          <p><a :href="item.url">{{item.url}}</a></p>
+          <div>{{item.body.slice(0,130)}}...</div>
+          <p>
+            <a :href="item.url">{{item.url}}</a>
+          </p>
         </li>
       </ul>
     </div>
@@ -18,8 +23,10 @@
 
 <script>
 export default {
-  async asyncData({app}) {
-    const items = await app.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js')
+  async asyncData({ app }) {
+    const items = await app.$axios.$get(
+      "https://qiita.com/api/v2/items?query=tag:nuxt.js"
+    )
     return {
       items
     }
@@ -46,5 +53,4 @@ li + li {
 p {
   margin: 8px 0;
 }
-
 </style>
