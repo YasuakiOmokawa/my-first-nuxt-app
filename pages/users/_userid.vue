043c86e882386ec8949fbbfc64b587ba51c909d2
面川泰明 <omokawa@senk-inc.co.jp>
2020-02-26 13:04:53 +0900

    Setting user page with ssr

--- pages/users/_userid.vue
+++ pages/users/_userid.vue
@@ -1,15 +1,64 @@
 <template>
-  <div>
-    <p>User ID: {{userId}}</p>
-  </div>
+  <section class="container">
+    <div>
+      <h3>{{user.id}}</h3>
+      <img :src="user.profile_image_url" witdh="120" />
+      <p>{{user.description || 'No description'}}</p>
+      <p>
+        <nuxt-link to="/">
+          <small>
+            <b>トップ</b>
+          </small>
+        </nuxt-link>
+      </p>
+      <h3>{{user.id}}さんの投稿一覧</h3>
+      <ul>
+        <li v-for="item in items" :key="item.id">
+          <h4>
+            <span>{{item.title}}</span>
+          </h4>
+          <div>{{item.body.slice(0, 130)}}...</div>
+          <p>
+            <a target="_blank" :href="item.url">{{item.url}}</a>
+          </p>
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      userId: this.$route.params.userid
-    }
+  async asyncData({ route, app }) {
+    const user = await app.$axios.$get(
+      `https://qiita.com/api/v2/users/${route.params.userid}`
+    )
+    const items = await app.$axios.$get(
+      `https://qiita.com/api/v2/items?query=user:${route.params.userid}`
+    )
+
+    return { user, items }
   }
 }
 </script>
+
+<style scoped>
+.container {
+  min-height: 100vh;
+  padding: 16px;
+}
+
+h3 {
+  margin: 16px 0;
+  padding: 8px 0;
+  border-bottom: solid 1px #e5e5e5;
+}
+
+li + li {
+  margin: 16px 0;
+}
+
+p {
+  margin: 8px 0;
+}
+</style>
