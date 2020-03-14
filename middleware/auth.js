e95d467ff942eb76635ea9938fa10ffa6c7ae4d6
面川泰明 <omokawa@senk-inc.co.jp>
2020-03-14 16:37:48 +0900

    Define middleware
    For authorized route

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,1 +1,17 @@
-export default () => {};
+import Cookies from "universal-cookie";
+
+export default ({ req, route, redirect }) => {
+  console.log(route.path);
+  if (["/"].includes(route.path)) {
+    return;
+  }
+  const cookies = req ? new Cookies(req.headers.cookie) : new Cookies();
+  const credential = cookies.get("credential");
+
+  if (credential && route.path === "/login") {
+    return redirect("/");
+  }
+  if (!credential && route.path !== "/login") {
+    return redirect("/login");
+  }
+};
--- nuxt.config.js
+++ nuxt.config.js
@@ -22,6 +22,9 @@ module.exports = {
   env: {
     QIITA_TOKEN: process.env.QIITA_TOKEN
   },
+  router: {
+    middleware: ["auth"]
+  },
   /*
    ** Build configuration
    */
