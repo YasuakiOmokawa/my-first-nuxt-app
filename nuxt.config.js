adb352d081011542bd1c1104297afae5dfd59caf
面川泰明 <omokawa@senk-inc.co.jp>
2020-04-04 14:44:33 +0900

    Add common css layout

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,6 +27,10 @@ module.exports = {
   router: {
     middleware: ["auth"]
   },
+  css: [
+    'element-ui/lib/theme-chalk/index.css',
+    '~/assets/common.css'
+  ]
   /*
    ** Build configuration
    */
